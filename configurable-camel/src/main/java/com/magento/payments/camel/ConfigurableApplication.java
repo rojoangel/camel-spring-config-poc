ca77386243d680e38ec9fc57e60cfb1edcd61135
Angel Rojo Marijuan <rojo.angel@gmail.com>
2018-08-19 00:24:11 +0200

    clean up

--- configurable-camel/src/main/java/com/magento/payments/camel/ConfigurableApplication.java
+++ configurable-camel/src/main/java/com/magento/payments/camel/ConfigurableApplication.java
@@ -1,18 +1,9 @@
 package com.magento.payments.camel;
 
-import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
-@Configuration
-//@EnableAutoConfiguration
 @SpringBootApplication
-//@ImportResource({"classpath:META-INF/spring/camel-context.xml"})
 public class ConfigurableApplication {
 
     /**
