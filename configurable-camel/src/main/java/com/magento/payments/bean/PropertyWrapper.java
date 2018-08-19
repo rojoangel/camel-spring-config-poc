d97a3d54b2c7ce1971dce1ffdf2750c91ba42aa8
Angel Rojo Marijuan <rojo.angel@gmail.com>
2018-08-19 12:46:37 +0200

    make refresh work for beans by adding configuring the 'refresh' context, adding 'aop' and resolve properties using the '@Value' anotation

--- configurable-camel/src/main/java/com/magento/payments/bean/PropertyWrapper.java
+++ configurable-camel/src/main/java/com/magento/payments/bean/PropertyWrapper.java
@@ -1,9 +1,17 @@
 package com.magento.payments.bean;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class PropertyWrapper {
+
+    @Value("${health.response}")
     private String value;
 
+    public PropertyWrapper() {
+    }
+
     public PropertyWrapper(String value) {
+        super();
         this.value = value;
     }
 
