73ef82fb327d968f9a5d9f583e5a6c7e0098a90c
Angel Rojo Marijuan <rojo.angel@gmail.com>
2018-08-19 12:23:56 +0200

    add a second route using a bean

---
+++ configurable-camel/src/main/java/com/magento/payments/bean/PropertyWrapper.java
@@ -0,0 +1,13 @@
+package com.magento.payments.bean;
+
+public class PropertyWrapper {
+    private String value;
+
+    public PropertyWrapper(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
